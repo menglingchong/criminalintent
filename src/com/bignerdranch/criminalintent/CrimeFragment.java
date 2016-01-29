44285804d4e99dc2aa4b4afcf79fd83fdf698edc
mlc <282733193@qq.com>
2016-01-29 12:08:32 +0800

    升级Crime类，更新CrimeFragment的布局

--- src/com/bignerdranch/criminalintent/CrimeFragment.java
+++ src/com/bignerdranch/criminalintent/CrimeFragment.java
@@ -7,12 +7,18 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 
 	@Override
 	// ≈‰÷√Fragment µ¿˝
@@ -48,6 +54,21 @@ public class CrimeFragment extends Fragment {
 
 			}
 		});
+
+		mDateButton = (Button) view.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+
+		mSolvedCheckBox = (CheckBox) view.findViewById(R.id.crime_solved);
+		mSolvedCheckBox
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						mCrime.setSolved(isChecked);
+					}
+				});
 		return view;
 	}
 
