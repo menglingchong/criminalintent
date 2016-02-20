5b4cbf940fd46b4b6c48025409b65d3f87a0611b
mlc <282733193@qq.com>
2016-02-20 10:47:30 +0800

    定制列表项，创建列表项布局，创建定制列表项适配器。

--- src/com/bignerdranch/criminalintent/CrimeListFragment.java
+++ src/com/bignerdranch/criminalintent/CrimeListFragment.java
@@ -3,11 +3,15 @@ package com.bignerdranch.criminalintent;
 import java.util.ArrayList;
 
 import android.app.ListFragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class CrimeListFragment extends ListFragment {
 
@@ -21,15 +25,48 @@ public class CrimeListFragment extends ListFragment {
 		getActivity().setTitle(R.string.crimes_title);
 		mCrimes = CrimeLab.get(getActivity()).getCrimes();// 先获取CrimeLab单例，再获取其中的crime列表
 
-		ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
-				android.R.layout.simple_list_item_1, mCrimes);
+		// ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+		// android.R.layout.simple_list_item_1, mCrimes);
+		CrimeAdapter adapter = new CrimeAdapter(mCrimes);
 		setListAdapter(adapter);// setListAdapter()是一个ListFragment类的便利方法。可为内置ListView设置adapter
 	}
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		// TODO Auto-generated method stub
-		Crime c = (Crime) (getListAdapter()).getItem(position);// getListAdapter()是一个ListFragment类的便利方法
-		Log.d(TAG, c.getTitle() + " was clicked"); 				// 返回设置在ListFragment列表视图上的adapter。
+		// Crime c = (Crime) (getListAdapter()).getItem(position);//
+		// getListAdapter()是一个ListFragment类的便利方法
+		Crime c = ((CrimeAdapter) getListAdapter()).getItem(position);
+		Log.d(TAG, c.getTitle() + " was clicked"); // 返回设置在ListFragment列表视图上的adapter。
+	}
+
+	// 创建定制的adapter适配器
+	private class CrimeAdapter extends ArrayAdapter<Crime> {
+
+		public CrimeAdapter(ArrayList<Crime> crimes) {
+			super(getActivity(), 0, crimes);// 调用父类的构造方法来绑定Crime对象的数组列表
+		}
+
+		// 覆盖getView()方法，返回产生于定制布局的视图对象，并填充对应的Crime数据
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			if (convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(
+						R.layout.list_item_crime, null);// 从定制布局 产生一个新的视图对象
+			}
+			Crime c = getItem(position);
+			TextView titleTextView = (TextView) convertView
+					.findViewById(R.id.crime_list_item_titleTextView);
+			titleTextView.setText(c.getTitle());
+			TextView dateTextView = (TextView) convertView
+					.findViewById(R.id.crime_list_item_dateTextView);
+			dateTextView.setText(c.getDate().toString());
+			CheckBox solvedCheckBox = (CheckBox) convertView
+					.findViewById(R.id.crime_list_item_solvedCheckBox);
+			solvedCheckBox.setChecked(c.isSolved());
+			return convertView;// 将视图对象返回给listView
+		}
+
 	}
 }
